2f3994e54dfaf5a0bb9e91481fc3069b95e22020
thales-bpl <thales.bplima@gmail.com>
2022-09-20 01:16:55 -0300

    lint fix

--- src/jobs.py
+++ src/jobs.py
@@ -1,9 +1,9 @@
-# from functools import lru_cache
+from functools import lru_cache
 import csv
 
-# @lru_cache
+@lru_cache
 def read(file_path):
-    with open(file_path, encoding = "utf-8") as file:
+    with open(file_path, encoding="utf-8") as file:
         file_read = csv.DictReader(file, delimiter=",")
         csv_file_list = []
         for row in file_read:
@@ -12,4 +12,5 @@ def read(file_path):
         # print(csv_file_list[0])
         return csv_file_list
 
-read('src/jobs.csv')
+
+# read('src/jobs.csv')
