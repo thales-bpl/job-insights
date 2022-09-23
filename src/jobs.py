4b829002ea2fd27b9e71d85aceeb6670ab0ddb27
thales-bpl <thales.bplima@gmail.com>
2022-09-23 01:00:47 -0300

    impl: filter_by_job_type

--- src/jobs.py
+++ src/jobs.py
@@ -6,9 +6,10 @@ import csv
 def read(file_path):
     with open(file_path, mode="r") as file:
         jobs_dict = csv.DictReader(file, delimiter=",")
-        jobs_list = [jobs for jobs in jobs_dict]
-        # return jobs_list[0]
+        jobs_list = [job for job in jobs_dict]
         return jobs_list
+        # return jobs_list[0]
+        # return list(jobs_dict)
 
 
 # print(read('./jobs.csv'))
