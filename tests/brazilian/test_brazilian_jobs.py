57f9dcf2170ed74d129abeb0882962b429ebd26e
thales-bpl <thales.bplima@gmail.com>
2022-09-26 04:37:44 -0300

    test: sort_by & brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,6 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = {'title': 'Maquinista', 'salario': 2000, 'tipo': 'trainee'}
+    assert read_brazilian_file('tests/mocks/brazilians_jobs.csv')[0] == result
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,7 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = read('src/jobs.csv')
+    assert sort_by(jobs, 'min_salary') == 20000
