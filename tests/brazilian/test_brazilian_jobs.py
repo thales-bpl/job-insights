ff854962d7c886f2e7aeb1628cda999ccaad1f5e
thales-bpl <thales.bplima@gmail.com>
2022-09-26 04:58:56 -0300

    fix2: last tests

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,5 +2,5 @@ from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    result = {'title': 'Maquinista', 'salary': 2000, 'type': 'trainee'}
+    result = {'title': 'Maquinista', 'salary': '2000', 'type': 'trainee'}
     assert read_brazilian_file('tests/mocks/brazilians_jobs.csv')[0] == result
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,8 +1,9 @@
 from src.sorting import sort_by
 from src.jobs import read
 
+jobs = read('src/jobs.csv')
+
 
 def test_sort_by_criteria():
-    jobs = read('src/jobs.csv')
     sort_by(jobs, 'min_salary')
     assert jobs[0] == '19857'
