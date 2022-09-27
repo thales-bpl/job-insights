57df2b45857c65b7d532a11deb1e8b66ced98f1d
thales-bpl <thales.bplima@gmail.com>
2022-09-27 04:36:20 -0300

    impl: route(/job/<index>)

--- src/jobs.py
+++ src/jobs.py
@@ -8,8 +8,4 @@ def read(file_path):
         jobs_dict = csv.DictReader(file, delimiter=",")
         jobs_list = [job for job in jobs_dict]
         return jobs_list
-        # return jobs_list[0]
-        # return list(jobs_dict)
 
-
-# print(read('./jobs.csv'))
